cc1a341248bee4c9f71668d4d84c111e3b213f9d
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-16 01:37:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var name
    katzDeli.push(name)
    return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
   }
-
+ 
   function nowServing(katzDeli) {
     if (katzDeli.length > 0) {
       return `Currently serving ${katzDeli.shift()}.`
