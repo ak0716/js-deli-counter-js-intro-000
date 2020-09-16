669853df71729eb2ff88834fb2228c65d6c763b0
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-16 02:51:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var name
 let katzLine = []
 function currentLine(katzDeli) {
   for (let i = 0; i < katzDeli.length; i++) {
-    line.push(` ` + [i + 1] + `. ` + katzDeli[i])
+    katzLine.push(` ` + [i + 1] + `. ` + katzDeli[i])
   }
   if (katzDeli.length > 0) {
     return `The line is currently: ${katzLine}`
